c59b2af56d9d7d80ebdc9fbb243d800ab0aa793b
Braian-de-Liz <braian_l_silva@estudante.sesisenai.org.br>
2025-08-10 17:38:08 -0300

    é funcionou, era so o nome dos arquivos nos paths -_-

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Home from './pages/home/index.jsx'
-import './pages/home/index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import Home from './pages/home/index.jsx';
+import './pages/home/App.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Home />
   </StrictMode>,
-)
+);
